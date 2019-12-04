2d02460d003554d9c5752230c1f76eda58979179
thdtjsdn <ssw@b2link.co.kr>
2019-12-04 19:40:25 +0900

    d, n, u

--- root/bin_win/BUILD - ui_template.bat
+++ root/bin_win/BUILD - ui_template.bat
@@ -6,6 +6,8 @@ call "DELETE - 빌드 파일 삭제 - ui_template.bat"
 
 ..\..\..\TtwPlatform-Binary--Win\Binary\NodeJS\exes100.exe "BUILD - ui_template.js"
 
-call "COPY - ui_templatebat"
+call "XCOPY - ui_template.bat"
 
 call "DELETE - 빌드 파일 삭제 - ui_template.bat"
+
+cmd /k
